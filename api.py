3fb01c1d5bcc134d13c7418947f195e1ba53fd9b
Prafull <official.sonawaneprafull@gmail.com>
2023-12-30 20:11:43 +0530

    instagram chat analyzer

---
+++ api.py
@@ -0,0 +1,92 @@
+from json_to_df import JSON_to_DF
+
+import pandas as pd 
+from matplotlib import pyplot as plt
+
+import streamlit as st 
+st.set_page_config(layout="wide")
+
+st.title("Instagram Chat Analyzer :")
+st.text("get insights on your personal and group chat")
+
+# input json file from user
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+
+    # df = pd.read_csv("./main.csv") # eventually going to remove it
+
+    json_to_df = JSON_to_DF(data)
+
+    # working on json to df converter -> on file name json_to_df.py
+    df = json_to_df.converter() # this method will run and gives output in Dataframe format
+
+    total_users = sorted(json_to_df.total_users())
+    total_users.insert(0, "None")
+    total_users.insert(1, "Overall")
+
+    # ==================================================================
+
+    user = st.sidebar.selectbox(label="Select User", options=total_users)
+
+    if user != "None":
+        col1, col2 = st.columns(spec=2)
+
+        if user != "Overall":
+            df = df[df['users'] == user]
+
+        # Summery
+        with col1:
+            st.header("Message Count")
+            st.text(len(df['word_count']))
+
+            st.header("Word Count")
+            st.text(sum(df['word_count']))
+        with col2:
+            st.header("Reel Shared")
+            st.text(len(df[df['msgs'] == "reel"]))
+
+            st.header("Media Shared")
+            st.text(len(df[df['msgs'] == "media"]))
+
+        # most active user
+        if user == "Overall":
+            col3, col4 = st.columns(spec=2)
+            with col3:
+                st.header("Activity bar")
+                active = df['users'].value_counts()
+                fig, ax = plt.subplots()
+                ax.bar(active.index, height=active.values)
+                plt.xticks(rotation=90)
+                fig.set_size_inches(5.5, 2.5)
+                st.pyplot(fig)
+
+            with col4:
+                st.header("Active Percentage")
+                st.dataframe(round((df['users'].value_counts() / df.shape[0])*100,2).reset_index().rename(columns={"count" : "percentage"}))
+
+        # most common word used
+        col5, col6 = st.columns(spec=2)
+        with col5:
+            common = json_to_df.most_common_words(df, 10)
+            st.header("Most Common Word Used")
+            st.dataframe(common)
+
+            x = common['words']
+            y = common['count']
+
+            fig,ax = plt.subplots()
+            ax.barh(x, width=y)
+            fig.set_size_inches(5.5, 2.5)
+            plt.xticks(rotation=90)
+            st.pyplot(fig)
+        
+        with col6:
+            pass
+
+
+    if st.button("View Dataset"):
+        st.dataframe(df,hide_index=True)
+
---
+++ json_to_df.py
@@ -0,0 +1,67 @@
+import json
+import pandas as pd
+
+
+f = open("hinglish-stopword.txt","r")
+stopwords = [word.split("\n")[0] for word in f.readlines()]
+
+# helper functions
+# from nltk import word_tokenize
+from string import punctuation
+# from nltk.stem import PorterStemmer
+from collections import Counter
+
+# ps = PorterStemmer()
+
+class JSON_to_DF :
+    def __init__(self, data:str) -> None:
+        self.data = json.loads(data)
+
+    def converter(self) -> pd.DataFrame:
+        msg = self.data
+        
+        sender_name = []
+        sender_content = []
+        for m in msg['messages']:
+            
+            if "content" in m.keys():
+                sender_name.append(m['sender_name'].split(" ")[0])
+                if "sent an attachment." in m['content'] :
+                    sender_content.append("reel")
+                else:
+                    sender_content.append(m['content'])
+            else :
+                sender_name.append(m['sender_name'].split(" ")[0])
+                sender_content.append("media")
+        
+        df_dict = {
+            "users": sender_name,
+            "msgs" : sender_content,
+        }
+
+        df = pd.DataFrame(df_dict)
+        df['word_count'] = df['msgs'].apply(self.word_count)
+        return df
+    
+    def word_count(self,text : str):
+        if "reel" not in text and "media" not in text:
+            return len(text.split(" "))
+        else:
+            return 0
+
+    def total_users(self):
+        msg = self.data
+        total_users = [user['name'].split(" ")[0] for user in msg['participants']]
+        return total_users
+    
+    def most_common_words(self, df: pd.DataFrame, count:int=20):
+        y = []
+        for text in df['msgs']:
+            text = text.lower()
+            for word in text.split(" "):
+                if str.isalnum(word) and word not in punctuation and word not in stopwords:
+                    y.append(word)
+
+        counter = Counter(y)
+        return pd.DataFrame(counter.most_common(count), columns=['words','count'])
+    
